2fb4665ebc81f31b519fc4491dfb0370ba534e50
unknown <lee@lee-PC.hs.handsome.com.cn>
2012-06-19 15:25:48 +0800

    project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+One2one::Application.config.secret_token = '64f977cceb13dc3bf006424c9305a9f87c094eb0bf032dd5930555a655fa8481f598a81f612b84b102fc7bf6044c4dad3d1306a02ef7c3a0053590dd7a9b348f'
